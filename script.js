9a7863274fd6bbab022d72e0f7ffaac1605b35f4
paiva737 <rafaelpaiva636@gmail.com>
2024-09-18 22:49:47 -0300

    adicionando offcanvas e botões

--- script.js
+++ script.js
@@ -92,3 +92,20 @@ const darkMode = document.querySelector(".cabecalho__switch-input");
 darkMode.addEventListener("change", () => {
     document.documentElement.classList.toggle("light");
 });
+
+const botaoAvatar = document.querySelector(".cabecalho__avatar");
+const offcanvas = document.querySelector(".offcanvas");
+const botaoFechar = document.querySelector(".btn-close");
+
+botaoAvatar.addEventListener("click", (event) => {
+    event.preventDefault(); // Impede o comportamento padrão do link
+    
+    // Alterna as classes "show" e "offcanvas-end"
+    offcanvas.classList.toggle("show");
+    offcanvas.classList.toggle("offcanvas-end");
+});
+
+
+botaoFechar.addEventListener("click", () => {
+    offcanvas.classList.remove("show"); // Remove a classe "show" para esconder o offcanvas
+});
