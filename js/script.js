011d74b5548bfc57aae1c11e3a38ad1069e5c198
paiva737 <rafaelpaiva636@gmail.com>
2024-10-31 18:38:26 -0300

    finalizando parte dos videos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const containerVideos = document.querySelector(".videos__container");
 
 
+
 async function buscarEMostrarVideos(){
     try{
         const busca = await fetch("http://localhost:3000/videos");
@@ -99,14 +100,14 @@ const offcanvas = document.querySelector(".offcanvas");
 const botaoFechar = document.querySelector(".btn-close");
 
 botaoAvatar.addEventListener("click", (event) => {
-    event.preventDefault(); // Impede o comportamento padrão do link
+    event.preventDefault(); 
     
-    // Alterna as classes "show" e "offcanvas-end"
+   
     offcanvas.classList.toggle("show");
     offcanvas.classList.toggle("offcanvas-end");
 });
 
 
 botaoFechar.addEventListener("click", () => {
-    offcanvas.classList.remove("show"); // Remove a classe "show" para esconder o offcanvas
+    offcanvas.classList.remove("show"); 
 });
